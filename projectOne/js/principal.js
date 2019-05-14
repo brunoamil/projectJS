1cb5e94ab59f1d0d027dfb43174fc9b74b3b92f4
Bruno Lima <bruno-amil@hotmail.com>
2019-05-14 11:07:25 -0300

    fazendo calculo do imc com js puro

---
+++ projectOne/js/principal.js
@@ -0,0 +1,38 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Bruno Nutricionista";
+
+// calculando o IMC
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if(peso <= 0 || peso >= 1000) {
+    console.log("Peso invalido");
+    pesoEhValido = false;
+    tdImc.textContent = "peso invalido!";
+}
+
+if(altura <= 0 || altura >= 3.0) {
+    console.log("Altura invalida");
+    alturaEhValida = false;
+    tdImc.textContent = "altura invalida!";
+
+}
+
+if(pesoEhValido && alturaEhValida){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+
+
+
