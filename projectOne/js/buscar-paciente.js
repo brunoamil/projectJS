cb962a85b13318a5b48f06fb3b1f321fc899f5f6
Bruno Lima <bruno-amil@hotmail.com>
2019-05-16 14:29:14 -0300

    consumindo api dos pacientes no heroku

---
+++ projectOne/js/buscar-paciente.js
@@ -0,0 +1,29 @@
+var botaoAdicionar = document.querySelector("#buscar-paciente");
+botaoAdicionar.addEventListener("click", function(){
+    console.log("buscando paciente...");
+    //consumindo api 
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "http://api-pacientes.herokuapp.com/paciesntes");
+    xhr.addEventListener("load", function(){
+        var erroAjax = document.querySelector("#erro-ajax");
+
+        if(xhr.status == 200){
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+             
+            //para cada paciente ele vai adicionar na tabela.
+            pacientes.forEach(function(paciente){
+             adicionaPacienteNaTabela(paciente);
+     
+            });
+        } else {
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            erroAjax.classList.remove("invisivel");
+        }
+      
+    });
+    xhr.send();
+});
--- projectOne/js/form.js
+++ projectOne/js/form.js
@@ -7,7 +7,6 @@ botaoAdicionar.addEventListener("click", function(event) {
   var form = document.querySelector("#form-adiciona");
   var paciente = obtemPacienteDoFormulario(form);
 
-  var pacienteTr = montaTr(paciente);
 
   var erros = validaPaciente(paciente);
   console.log(erros);
@@ -15,13 +14,21 @@ botaoAdicionar.addEventListener("click", function(event) {
     exibeMensagensDeErro(erros);
     return;
   }
-  //colocando dentro da tabela
-  var tabela = document.querySelector("#tabela-pacientes");
-  tabela.appendChild(pacienteTr);
+  
+  adicionaPacienteNaTabela();
+  
   form.reset();
   var mensagensErro = document.querySelector("#mensagens-erro");
   mensagensErro.innerHTML = "";
 });
+
+function adicionaPacienteNaTabela(paciente){
+    var pacienteTr = montaTr(paciente);
+    //colocando dentro da tabela
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+
+}
 
 function exibeMensagensDeErro(erros) {
   var ul = document.querySelector("#mensagens-erro");
