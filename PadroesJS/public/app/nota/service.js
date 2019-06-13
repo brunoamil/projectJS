717d07f5f4e60fe172568f8778d627003234346a
bruno lima <bruno-amil@hotmail.com>
2019-06-13 00:00:15 -0300

    criacao da monada para tratamento de valores nulos

--- PadroesJS/public/app/nota/service.js
+++ PadroesJS/public/app/nota/service.js
@@ -1,18 +1,21 @@
 import { handleStatus } from "../utils/promise-helpers.js";
 import { partialize, pipe } from "../utils/operators.js";
+import { Maybe } from "../utils/maybe.js";
 
 const API = "http://localhost:3000/notas";
 
-const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
-const filterItemsByCode = (code, items) =>
-  items.filter(item => item.codigo == code);
-const sumItemsValue = items =>
-  items.reduce((total, item) => total + item.valor, 0);
+const getItemsFromNotas = notasM =>
+  notasM.map(notas => notas.$flatMap(nota => nota.itens));
+const filterItemsByCode = (code, itemsM) =>
+  itemsM.map(items => items.filter(item => item.codigo == code));
+const sumItemsValue = itemsM =>
+  itemsM.map(items => items.reduce((total, item) => total + item.valor, 0));
 
 export const notasService = {
   listAll() {
     return fetch(API)
       .then(handleStatus)
+      .then(notas => Maybe.of(notas))
       .catch(err => {
         console.log(err);
         return Promise.reject("nao foi possivel obter as notas fiscais");
@@ -26,6 +29,8 @@ export const notasService = {
       filterItems,
       sumItemsValue
     );
-    return this.listAll().then(sumItems);
+    return this.listAll()
+      .then(sumItems)
+      .then(result => result.getOrElse(0));
   }
 };
