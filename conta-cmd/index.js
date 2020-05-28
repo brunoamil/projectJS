fa396faae6755e1813165bbc5bcd531a0087ed8e
Bruno Lima <bruno-amil@hotmail.com>
2020-05-28 11:48:49 -0300

    trabalhando na criacao de uma conta bank de js teste

---
+++ conta-cmd/index.js
@@ -0,0 +1,23 @@
+import {Cliente} from './Cliente.js';
+import {ContaCorrente} from './ContaCorrente.js';
+
+const cliente1 =  new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 123456;
+
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 123456;
+
+const ContaCorrenteRicardo = new ContaCorrente();
+ContaCorrenteRicardo.agencia = 101;
+ContaCorrenteRicardo.cliente = cliente1;
+ContaCorrenteRicardo.depositar(500);
+
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+
+ContaCorrenteRicardo.transferir(200, conta2);
+
+console.log(conta2);
+console.log(ContaCorrenteRicardo);
