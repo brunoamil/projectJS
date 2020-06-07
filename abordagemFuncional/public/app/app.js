5f32266e3a2751583437f0317a27a9cfb3770171
Bruno Lima <bruno-amil@hotmail.com>
2020-06-07 20:58:53 -0300

    trabalhando com api

--- abordagemFuncional/public/app/app.js
+++ abordagemFuncional/public/app/app.js
@@ -1,3 +1,18 @@
+import { log } from './utils/promise-helpers.js';
+import './utils/array-helpers.js';
+import { notasService as service } from './nota/service.js';
+
+
+
 document
 .querySelector("#myButton")
-.onclick = () => alert("Oi");
+.onclick = () => 
+    service
+    .sumItems('2143')
+    .then(console.log)
+    .catch(console.log);
+
+  
+
+
+   
