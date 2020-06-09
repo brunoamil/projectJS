80456d477db04e2e2bfdf1d95a7a982299b9e2ba
Bruno Lima <bruno-amil@hotmail.com>
2020-06-08 22:01:49 -0300

    trabalhando com promises

--- abordagemFuncional/public/app/app.js
+++ abordagemFuncional/public/app/app.js
@@ -1,16 +1,25 @@
-import { log } from './utils/promise-helpers.js';
+import { log, timeoutPromise, retry } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
+import { takeUntill, debounceTime, partialize, pipe } from './utils/operators.js';
 
+
+const operatios = pipe(
+    partialize(takeUntill, 3),
+    partialize(debounceTime,500)
+);
+
+const action = operatios(() =>
+        retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
+        .then(console.log)
+        .catch(console.log)
+    );
 
 
 document
 .querySelector("#myButton")
-.onclick = () => 
-    service
-    .sumItems('2143')
-    .then(console.log)
-    .catch(console.log);
+.onclick = action; 
+    
 
   
 
--- abordagemFuncional/public/app/nota/service.js
+++ abordagemFuncional/public/app/nota/service.js
@@ -1,19 +1,31 @@
 import { handleStatus } from '../utils/promise-helpers.js';
+import { partialize, compose, pipe } from '../utils/operators.js';
+
 
 const API = 'http://localhost:3000/notas';
 
-const sumItems = code => notas => notas
-                .$flatMap(nota => nota.itens)
-                .filter(item => item.codigo == code )
-                .reduce((total, item) => total + item.valor, 0);
-
+const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
+const filterItemsByCode = (code, items) => items.filter(item => item.codigo == code );
+const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
+                
 export const notasService = {
 
     listAll(){
-        return fetch(API).then(handleStatus);
+        return fetch(API)
+        .then(handleStatus)
+        .catch(err => {
+            console.log(err);
+            return Promise.reject("Nao foi possivel obter notas fiscais");
+        });
     },
 
     sumItems(code) {
-        return this.listAll().then(sumItems(code));
+        const filterItems = partialize(filterItemsByCode, code);
+        const sumItems = pipe(
+            getItemsFromNotas,
+            filterItems,
+            sumItemsValue)
+        return this.listAll()
+            .then(sumItems);
     }
 };
